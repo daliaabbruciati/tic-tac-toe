27e0c207ee6e95cdf2c4578807c3e14bd2c67201
Dalia Abbruciati <dalia@netstrada.it>
2022-05-30 00:39:01 +0200

    Add local storage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import {Game} from "./components/game";
 
 function App() {
-    console.log('render in App.js')
     return (
         <div className="App">
             <h2>Tic tac toe</h2>
--- src/components/game.jsx
+++ src/components/game.jsx
@@ -1,60 +1,54 @@
-import React, {useState,useEffect} from 'react'
+import React, {useState, useEffect, useRef} from 'react'
 import {Cell} from "./cell";
 
-// const [board, setBoard] = useState(initialBoard)
-// const [turn, setTurn] = useState('X')
-// const [winner, setWinner] = useState('')
-// const [isBoardFull, setIsBoardFull] = useState(false)
-// const [history, setHistory] = useState([0])
-
-
-let boot = false;
 
 const PL1 = 'X'
 const PL2 = 'O'
 
+const initialBoard = Array(9).fill('')
+
+const initialState = {
+    board: initialBoard,
+    turn: PL1,
+    winner: '',
+    isBoardFull: false,
+    history: [0],
+    newHistoryValue: [0]
+}
+
+const useLocalStorageState = (key, initialValue) => {
+    const [state, setState] = useState(() => {
+        const localData = localStorage.getItem(key)
+        if (localData) {
+            try {
+                return JSON.parse(localData)
+            } catch (error) {
+                localStorage.removeItem(key)
+            }
+        }
+        return initialValue
+    })
+
+    const prevKeyRef = useRef(key)
+
+    useEffect(() => {
+        const prevKey = prevKeyRef.current
+        if (prevKey !== key) {
+            localStorage.removeItem(prevKey)
+        }
+        prevKeyRef.current = key
+        localStorage.setItem(key, JSON.stringify(state))
+    }, [key, state])
+
+    return [state, setState]
+}
 
 
 export const Game = () => {
 
-    const initialBoard = Array(9).fill('')
-
-    const initialState = {
-        board: initialBoard,
-        turn: PL1,
-        winner: '',
-        isBoardFull: false,
-        history: [0],
-        newHistoryValue: initialBoard
-    }
-
-    const [gameState, setGameState] = useState(initialState)
-
-    // const [gameStateStorage, setGameStateStorage] = useState(() => {
-    //     const localData = localStorage.getItem('gameState')
-    //     const initialValue = JSON.parse(localData)
-    //     if(initialValue) return setGameState(initialValue)
-    // })
+    const [gameState, setGameState] = useLocalStorageState('gameState', initialState)
 
     const {board, turn, winner, isBoardFull, history, newHistoryValue} = gameState
-
-    useEffect(() => {
-      console.log('useeffect 1');
-      if (!boot) {
-        boot = true
-        return
-      }
-      localStorage.setItem('gameState', JSON.stringify(gameState))
-    }, [gameState])
-
-
-    useEffect(() => {
-      console.log('useeffect 2')
-      const localData = localStorage.getItem('gameState')
-      const initialValue = JSON.parse(localData)
-      if (initialValue) return setGameState(initialValue)
-    }, [])
-
 
     const checkWinner = (index) => {
         const combos = [
@@ -84,55 +78,64 @@ export const Game = () => {
         }
     }
 
-    const onClickCell = (index) => {
+
+    const onClickBoard = (index) => {
 
         if (board[index] !== '') {
-            alert('Already clicked')
-            return
+            return alert('Already clicked')
         }
 
         if (turn === PL1) {
             board[index] = PL1
-            setGameState({...gameState,
+            setGameState({
+                ...gameState,
                 turn: PL2,
-                history: [...history, [...board]]})
+                history: [...history, [...board]],
+                newHistoryValue: history.slice(0, index + 1),
+            })
         } else {
             board[index] = PL2
-            setGameState({...gameState,
+            setGameState({
+                ...gameState,
                 turn: PL1,
-                history: [...history, [...board]]})
+                history: [...history, [...board]],
+                newHistoryValue: history.slice(0, index + 1)
+            })
         }
-
 
         checkWinner(index);
 
         console.log(history)
         console.log(newHistoryValue)
+        console.log(board)
     }
 
     const onClickShowHistory = (index) => {
+
         setGameState({
             ...gameState,
             board: JSON.parse(JSON.stringify(history[index])),
-            history: history.slice(0, index + 1),
+            turn: index % 2 === 0 ? PL1 : PL2,
+            winner: '',
+            newHistoryValue: history.slice(0, index + 1),
+            // history: history.slice(0, index + 1)
         })
 
+
         //TODO:
-        // 1. aggiungere il local storage
-        // 2. per troncare la storia quando clicco sul bottone indice della storia,
+        // 1. per troncare la storia quando clicco sul bottone indice della storia,
         // ma devo fare in modo che accada solo quando riclicco sulla board
-        // setGameState({..gameState, history: history.slice(0,indexClicked) });
-        // 3. sistemare caso in cui clicco sull'indice 0 e mi dice 'already clicked'
+        // setGameState({..gameState, history: history.slice(0, index + 1) });
+        // 2. sistemare caso in cui clicco sull'indice 0 e mi dice 'already clicked'
 
 
         console.log(history[index])
         console.log(newHistoryValue)
+        console.log(board)
         console.log(index)
     }
 
-    const onClickRestart = () => {
-        setGameState(initialState)
-    }
+    const onClickRestart = () => setGameState(initialState)
 
 
     console.log('render in Game.js')
@@ -140,9 +143,18 @@ export const Game = () => {
         <div className='board'>
             {winner ? <h2>{winner}</h2> : <h2>Player {turn}, it's your turn</h2>}
             {isBoardFull && <h2>Nobody won</h2>}
-            <Cell board={board} onClick={winner ? (e) => e.preventDefault : onClickCell}/>
+            <Cell board={board} onClick={winner ? (e) => e.preventDefault : onClickBoard}/>
+            <p>history</p>
             <div className='board__snapshot'>
-                {history?.map((item, index) => (
+                {history.map((item, index) => (
+                        <button key={index} className='board__btn-snapshot'
+                                onClick={() => onClickShowHistory(index)}>{index}</button>
+                    )
+                )}
+            </div>
+            <p>new history value</p>
+            <div className='board__snapshot'>
+                {newHistoryValue.map((item, index) => (
                         <button key={index} className='board__btn-snapshot'
                                 onClick={() => onClickShowHistory(index)}>{index}</button>
                     )
